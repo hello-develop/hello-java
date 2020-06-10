04344741970accbf4483101569043a61ef52412d
cc <cc@znvis.cn>
2020-06-10 10:46:42 +0800

    init

--- src/main/java/cn/znvis/hello/java/Hello.java
+++ src/main/java/cn/znvis/hello/java/Hello.java
@@ -1,5 +1,8 @@
 package cn.znvis.hello.java;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * DESC
  *
@@ -14,5 +17,13 @@ public class Hello {
 
     public void hello2() {
         System.out.println(">>> hello2");
+    }
+
+    public static void main(String[] args) {
+        Map<String, Boolean> map = new HashMap<>();
+        map.put("cc", true);
+        if (map.getOrDefault("dd", true)) {
+            System.out.println(">>> ok");
+        }
     }
 }
